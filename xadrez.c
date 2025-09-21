73c2529b83c4a8cfa97caecb439fce95543559d7
Cristiano Maciel <cristiano_maciel@hotmail.com>
2025-09-21 12:29:32 +0000

    Movimentação de peças de xadrez, desafio novato concluído.

--- xadrez.c
+++ xadrez.c
@@ -2,12 +2,23 @@
 
 int main() {
     
-    int t, b, r;
-
-// Movimentação TORRE | Usando: "FOR"
-    for (int t = 0; t < 5; t++)
-        {printf("Direita\n");} // Imprime a direção do movimento da TORRE
+    int t = 0, b = 0, r = 0;
 
 
+    printf("Movimentação TORRE:\n");
+        for (t; t < 5; t++)
+        {printf("Direita\n");}
+
+    printf("\nMovimentação BISPO:\n");
+        do{
+        printf("Cima, Direita\n");
+        b++;
+        } while (b < 5);
+
+    printf("\nMovimentação RAINHA:\n");//OBS: é impossível a rainha se movimentar 8 casas para a esquerda pois o tabuleiro
+        while (r < 8)                  //tem apenas 8 colunas.. então para ele andar "oito casas para a esquerda"
+        {printf("Esquerda\n"); r++;}   //conforme enunciado, a rainha precisaria estar na casa 9 (que não existe).
+
+    
     return 0;
 }
